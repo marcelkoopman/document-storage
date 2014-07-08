b6f8b05ad32cd8656782aa4637e73b215f0eab46
Marcel Koopman <marcel.koopman@gmail.com>
2014-07-08 20:56:42 +0200

    Initial commit

--- bundles/document-storage-core/src/main/java/com/github/document/storage/core/DocumentStorageCoreActivator.java
+++ bundles/document-storage-core/src/main/java/com/github/document/storage/core/DocumentStorageCoreActivator.java
@@ -2,19 +2,31 @@ package com.github.document.storage.core;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogService;
 
 public class DocumentStorageCoreActivator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
-
+		LogService log = getLogService(context);
+		log.log(LogService.LOG_INFO, "Bundle started!");
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
+		LogService log = getLogService(context);
+		log.log(LogService.LOG_INFO, "Bundle stopped!");
+	}
 
+	private LogService getLogService(BundleContext context) {
+		final ServiceReference<LogService> ref = context
+				.getServiceReference(LogService.class);
+		LogService log = null;
+		if (ref != null) {
+			log = context.getService(ref);
+		}
+		return log;
 	}
 
 }

8088646280385664dab32203da2636f7188b7cd5
Marcel Koopman <marcel.koopman@gmail.com>
2014-07-08 20:47:25 +0200

    Initial commit

---
+++ bundles/document-storage-core/src/main/java/com/github/document/storage/core/DocumentStorageCoreActivator.java
@@ -0,0 +1,20 @@
+package com.github.document.storage.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class DocumentStorageCoreActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ bundles/document-storage-core/src/main/java/com/github/document/storage/core/impl/DocumentStorageCoreImpl.java
@@ -0,0 +1,12 @@
+package com.github.document.storage.core.impl;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Provides;
+
+import com.github.document.storage.core.DocumentStorageCore;
+
+@Component
+@Provides
+public class DocumentStorageCoreImpl implements DocumentStorageCore {
+
+}
